c10638079179c11aa486797b460f07069a8a59db
Hung Truong <hungtruongquoc@gmail.com>
2024-07-18 20:35:05 -0500

    Added logging

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Setup Morgan for HTTP request logging
+app.use(morgan('combined'));
+
 const apiProxy = createProxyMiddleware({
     target: process.env.TARGET_URL,
-    changeOrigin: true
+    changeOrigin: true,
+    logLevel: 'debug', // Enable debug level logging
+    logProvider: () => console // Use the console for logging
 });
 
 app.use('/', apiProxy);
